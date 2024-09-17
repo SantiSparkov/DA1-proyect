75c34c6300cb6771094418b7c05ce5b203b346f9
Francisco Sosa <fran-sosaaa@hotmail.com>
2024-09-17 11:23:12 -0300

    Test green: create user entity

---
+++ TaskPanel/Models/Entity/User.cs
@@ -0,0 +1,18 @@
+namespace TaskPanel.Models.Entity;
+
+public class User
+{
+    public string Name { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public DateTime BrithDate { get; set; }
+    
+    public bool IsAdmin { get; set; }
+    
+    public Trash Trash { get; set; }
+}
--- TaskPanelTest/UserEntityTest/UserEntityTest.cs
+++ TaskPanelTest/UserEntityTest/UserEntityTest.cs
@@ -17,7 +17,7 @@ public class UserEntityTest
             BrithDate = new DateTime(2008, 6, 1, 7, 47, 0),
             Password = "password",
             IsAdmin = false,
-            //Trash = new Trash()
+            Trash = new Trash()
         };
         
         //Act 
@@ -29,5 +29,6 @@ public class UserEntityTest
         Assert.AreEqual(new DateTime(2008, 6, 1, 7, 47, 0), user.BrithDate);
         Assert.AreEqual("password", user.Password);
         Assert.IsFalse(user.IsAdmin);
+        Assert.IsNotNull(user.Trash);
     }
 }
