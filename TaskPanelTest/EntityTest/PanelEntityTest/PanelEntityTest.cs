4a026d0801f1adf3de266000fed449ffa20bd38c
Francisco Suarez <franciscoysuarez@hotmail.com>
2024-09-18 21:21:51 -0300

    Test Panel - Red case

---
+++ TaskPanelTest/EntityTest/PanelEntityTest/PanelEntityTest.cs
@@ -0,0 +1,29 @@
+namespace TaskPanelTest.EntityTest.TaskEntityTest;
+using TaskPanel.Models.Entity;
+using TaskPriority = TaskPanel.Models.Entity.Task.TaskPriority;
+
+[TestClass]
+public class PanelEntityTest
+{
+    [TestMethod]
+    public void CreatePanelTest()
+    {
+        //Arrange 
+        Panel panel = new Panel()
+        {
+            Id = 1,
+            Name = "Panel1",
+            Team = new Team(),
+            Description = "description",
+            Tasks = new List<Task>()
+        };
+        
+        // Assert
+        Assert.AreEqual(1, panel.Id);
+        Assert.AreEqual("Panel1", panel.Name);
+        Assert.IsNotNull(panel.Team);
+        Assert.AreEqual("description", panel.Description);
+        Assert.IsNotNull(panel.Tasks);
+
+    }
+}
