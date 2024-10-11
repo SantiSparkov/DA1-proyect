74e37237c6309c382fed364ad35b245d1ae5d800
SantiSparkov <SS251817@fi365.ort.edu.uy>
2024-10-11 15:02:42 -0300

    Methods to implement

--- TaskPanelLibrary/Service/Interface/ITeamService.cs
+++ TaskPanelLibrary/Service/Interface/ITeamService.cs
@@ -5,5 +5,21 @@ namespace TaskPanelLibrary.Service.Interface;
 
 public interface ITeamService
 {
+    public Team CreateTeam(Team team, int userId);
     
+    public Team DeleteTeam(Team team , int userId);
+    
+    public Team UpdateTeam(Team team, int userId);
+    
+    public Team GetTeamById(int id);
+    
+    public List<Team> GetAllTeams();
+    
+    public void AddUserToTeam(int userId, Team team);
+
+    public void RemoveUserFromTeam(int userId, Team team);
+    
+    public void AddPanelToTeam(int panelId, Team team);
+    
+    public void RemovePanelFromTeam(int panelId, Team team);
 }
--- TaskPanelLibrary/Service/TeamService.cs
+++ TaskPanelLibrary/Service/TeamService.cs
@@ -1,8 +1,64 @@
+using TaskPanelLibrary.Repository;
 using TaskPanelLibrary.Service.Interface;
+using Team = TaskPanelLibrary.Entity.Task;
 
 namespace TaskPanelLibrary.Service;
 
 public class TeamService : ITeamService
 {
+    private readonly TeamRepository _teamRepository;
+    private UserService _userService;
+    private PanelService _panelService;
     
+    public TeamService(TeamRepository teamRepository, UserService userService, PanelService panelService)
+    {
+        _teamRepository = teamRepository;
+        _userService = userService;
+        _panelService = panelService;
+    }
+    
+    public Team CreateTeam(Team team, int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Team DeleteTeam(Team team, int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Team UpdateTeam(Team team, int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Team GetTeamById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Team> GetAllTeams()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddUserToTeam(int userId, Team team)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveUserFromTeam(int userId, Team team)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddPanelToTeam(int panelId, Team team)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemovePanelFromTeam(int panelId, Team team)
+    {
+        throw new NotImplementedException();
+    }
 }
