82adcb702ba8b52ed22361b96f11e02a3a2c11bb
Francisco Suarez <franciscoysuarez@hotmail.com>
2024-10-13 01:06:26 -0300

    test (GREEN): add Login model implementation

---
+++ TaskPanelLibrary/Entity/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskPanelLibrary.Entity;
+
+public class Login
+{
+    [Required(ErrorMessage = "Email must not be empty.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    [RegularExpression(@"^[^@]+@[^@]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email must contain both '@' and '.'")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password must not be empty.")]
+    public string Password { get; set; }
+}
