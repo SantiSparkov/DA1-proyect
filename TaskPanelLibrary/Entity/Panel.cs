337104e04b996fa61c2fda080c1663d16197032e
Francisco Suarez <franciscoysuarez@hotmail.com>
2024-10-15 20:13:20 -0300

    feat: add front validations

--- TaskPanelLibrary/Entity/Panel.cs
+++ TaskPanelLibrary/Entity/Panel.cs
@@ -1,13 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskPanelLibrary.Entity;
 
 public class Panel
 {
     public int Id { get; set; }
     
+    [Required(ErrorMessage = "The name is required")]
     public string Name { get; set; }
     
+    [Required(ErrorMessage = "The team is required")]
     public Team Team { get; set; }
     
+    [Required(ErrorMessage = "The description is required")]
     public string Description { get; set; }
     
     public List<Task> Tasks { get; set; }
