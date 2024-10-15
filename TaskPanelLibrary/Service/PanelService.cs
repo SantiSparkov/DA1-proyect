5a31b90e2730a05ce632d691e9973128bcdf5bc4
SantiSparkov <SS251817@fi365.ort.edu.uy>
2024-10-15 19:36:43 -0300

    validation refactors

--- TaskPanelLibrary/Service/PanelService.cs
+++ TaskPanelLibrary/Service/PanelService.cs
@@ -21,6 +21,8 @@ public class PanelService : IPanelService
 
     public Panel CreatePanel(Panel panel)
     {
+        if (!IsValidPanel(panel))
+            throw new PanelNotValidException("Panel is not valid");
         panel.Id = _panelRepository.Count() + 1;
         return _panelRepository.AddPanel(panel);
     }
@@ -99,8 +101,15 @@ public class PanelService : IPanelService
         return _panelRepository.GetAll();
     }
     
-    private bool IsValidTask(Task task)
+    private bool IsValidPanel(Panel panel)
     {
-        return task != null && !string.IsNullOrEmpty(task.Title) && !string.IsNullOrEmpty(task.Description);
+        if (panel == null)
+            throw new PanelNotValidException("Panel is null");
+        if (string.IsNullOrEmpty(panel.Name))
+            throw new PanelNotValidException("Panel name is null or empty");
+        if (string.IsNullOrEmpty(panel.Description))
+            throw new PanelNotValidException("Panel description is null or empty");
+        
+        return true;
     }
 }
